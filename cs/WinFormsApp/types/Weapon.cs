4bf3afa0a97ca4a61b085e52463189bc7596df87
m1ss1onBest <m1poosthbexxxx@gmail.com>
2024-08-24 23:11:45 +0300

    create-weapon.cs

---
+++ cs/WinFormsApp/types/Weapon.cs
@@ -0,0 +1,33 @@
+﻿// ReSharper disable once IdentifierTypo
+namespace windowsformsapp1.types
+{
+    public class Weapon
+    {
+        private string Name { set; get; } = "no-name-specified";
+        private string Caliber { set; get; } = "no-caliber-specified";
+        private int BarrelLength { set; get; }
+
+        public Weapon()
+        {
+            BarrelLength = 0;
+        }
+
+        public Weapon(string name, string caliber, int barrelLength)
+        {
+            Name = name;
+            Caliber = caliber;
+            BarrelLength = barrelLength;
+        }
+        
+        public Weapon(Weapon weapon)
+        {
+            Name = weapon.Name;
+            Caliber = weapon.Caliber;
+            BarrelLength = weapon.BarrelLength;
+        }
+
+        public string[] Format() =>
+            new[] { Name, Caliber, BarrelLength.ToString()};
+
+    }
+}
