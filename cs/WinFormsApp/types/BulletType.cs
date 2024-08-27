99268d3bc15dc7a6083a40ede0e238e175c04642
m1ss1onBest <m1poosthbexxxx@gmail.com>
2024-08-27 20:03:35 +0300

    add-bullet-type

---
+++ cs/WinFormsApp/types/BulletType.cs
@@ -0,0 +1,34 @@
+﻿namespace WinFormsApp.types
+{
+    public class BulletType
+    {
+        private string Name { set; get; } = "no-name-specified";
+        private int Damage { set; get; } = 1;
+        private int ArmorPiercing { set; get; } = 0;
+        private int Speed { set; get; } = 1;
+        private bool Tracer { set; get; } = false;
+
+        public BulletType() 
+        {
+            
+        }
+
+        public BulletType(string name, int damage, int armorPiercing, int speed, bool tracer)
+        {
+            Name = name;
+            Damage = damage;
+            ArmorPiercing = armorPiercing;
+            Speed = speed;
+            Tracer = tracer;
+        }
+
+        public BulletType(BulletType bulletType)
+        {
+            Name = bulletType.Name;
+            Damage = bulletType.Damage;
+            ArmorPiercing = bulletType.ArmorPiercing;
+            Speed = bulletType.Speed;
+            Tracer = bulletType.Tracer;
+        }
+    }
+}
---
+++ cs/WinFormsApp/types/WeaponType.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable once IdentifierTypo
+
+using System;
+
+namespace WinFormsApp.types
+{
+    public class WeaponType
+    {
+        private string Name { set; get; } = "no-name-specified";
+        private BulletType Bullet { set; get; }
+        private int BarrelLength { set; get; } = 1;
+
+        public WeaponType()
+        {
+            
+        }
+
+        public WeaponType(string name, int barrelLength, BulletType bullet)
+        {
+            Name = name;
+            BarrelLength = barrelLength;
+            Bullet = bullet;
+        }
+        
+        public WeaponType(WeaponType weaponType)
+        {
+            Name = weaponType.Name;
+            BarrelLength = weaponType.BarrelLength;
+            Bullet = weaponType.Bullet;
+        }
+    }
+}
