5cf25b9dac07c8aa735dd19d57bce7dc332a2ffd
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-09-05 05:42:47 +0300

    feature/uint256type

---
+++ cs/WinFormsApp/Data/Tests/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using WinFormsApp.Data;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace WinFormsApp.Tests
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Test(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void ShouldCreateEmptyUint256()
+        {
+            UInt256 uint256 = new UInt256();
+            Assert.Equal(uint256.Data, new uint[8]);
+        }
+
+        [Fact]
+        public void ShouldParseCorrectly()
+        {
+            uint[] values =
+                { 0x15ff3ab1, 0xaaf3945, 0xfabcd320, 0x16f24ec, 0x111ffa11, 0x44122222, 0x3333ffab, 0x2342f2cc };
+
+            string value = String.Empty;
+
+            for (int i = 0; i < 8; i++)
+            {
+                value += values[i].ToString("X8");
+            }
+            
+            Assert.Equal(64, value.Length);
+            _testOutputHelper.WriteLine($@"0x{value}");
+        }
+    }
+}
---
+++ cs/WinFormsApp/Data/UInt256.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WinFormsApp.Data
+{
+    public class UInt256
+    {
+        private uint[] _data;
+        public uint[] Data
+        {
+            set => _data = value;
+            get => _data;
+        }
+
+        public UInt256() => _data = new uint[8];
+        public UInt256(string hex) => _data = FromHex(hex);
+        public UInt256(UInt256 num) => _data = num._data;
+
+        public static uint[] FromHex(string hex)
+        {
+            if (hex.StartsWith("0x")) hex = hex.Substring(2);
+            if (hex.Length != 64)
+                throw new ArgumentException("Invalid hex string length. Must be 64 characters.");
+
+            uint[] data = new uint[8];
+
+            for (var i = 0; i < 8; i++)
+            {
+                data[i] = uint.Parse(
+                    hex.Substring(i * 8, 8),
+                    System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return data;
+        }
+
+        public int Bits() => 256;
+    }
+
+}
