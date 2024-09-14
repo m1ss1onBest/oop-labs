6ac63b00a06a3eddcb98da8141a4c290dfd6cac8
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-09-14 19:01:47 +0300

    small fixes

--- cpp/3_2_inheritance/main.cpp
+++ cpp/3_2_inheritance/main.cpp
@@ -1,35 +1,30 @@
-#include <chrono>
 #include <iostream>
 #include <string>
+#include <memory>
 
 class PrintPublication {
 protected:
-    std::pmr::string title;
+    std::string title;
     int year;
 
 public:
     PrintPublication() : title("Title"), year(0) {}
-    PrintPublication(const std::string& title, int year):
-    title(title),
-    year(year) {
-
-    }
+    PrintPublication(const std::string& title, int year)
+    : title(title), year(year) {}
 
     virtual ~PrintPublication() = default;
+
     virtual void printDetails() const {
         std::cout << "Title: " << title << ", Year: " << year << std::endl;
     }
-
 };
 
 class Book : public PrintPublication {
     std::string author;
-public:
-    Book(const std::string& title, int year, const std::string& author) :
-    PrintPublication(title, year),
-    author(author) {
 
-    }
+public:
+    Book(const std::string& title, int year, const std::string& author)
+    : PrintPublication(title, year), author(author) {}
 
     void printDetails() const override {
         PrintPublication::printDetails();
@@ -41,11 +36,8 @@ class Textbook : public Book {
     std::string subject;
 
 public:
-    Textbook(const std::string& title, int year, const std::string& author, const std::string& subject):
-    Book(title, year, author),
-    subject(subject) {
-
-    }
+    Textbook(const std::string& title, int year, const std::string& author, const std::string& subject)
+    : Book(title, year, author), subject(subject) {}
 
     void printDetails() const override {
         Book::printDetails();
@@ -55,31 +47,28 @@ public:
 
 class Magazine : public PrintPublication {
     int issueNumber;
-public:
-    Magazine(const std::string& title, int year, int issueNumber):
-    PrintPublication(title, year),
-    issueNumber(issueNumber) {
 
-    }
+public:
+    Magazine(const std::string& title, int year, int issueNumber)
+    : PrintPublication(title, year), issueNumber(issueNumber) {}
 
     void printDetails() const override {
         PrintPublication::printDetails();
-        std::cout << "Issua Number: " << issueNumber << std::endl;
+        std::cout << "Issue Number: " << issueNumber << std::endl;
     }
 };
 
 int main() {
-    PrintPublication* publications[4] {
-        new Book("Harry Harrington", 1991, "John Wednesday"),
-        new Textbook("Introduction to Algorithms", 2009, "Thomas H Cormen", "Algorithms"),
-        new Magazine("National Geographic", 2024, 9),
-        new PrintPublication("Generic Publication", 2024)
+    std::unique_ptr<PrintPublication> publications[4] {
+        std::make_unique<Book>("Harry Harrington", 1991, "John Wednesday"),
+        std::make_unique<Textbook>("Introduction to Algorithms", 2009, "Thomas H Cormen", "Algorithms"),
+        std::make_unique<Magazine>("National Geographic", 2024, 9),
+        std::make_unique<PrintPublication>("Generic Publication", 2024)
     };
 
-    for (auto & publication : publications) {
+    for (const auto& publication : publications) {
         publication->printDetails();
         std::cout << std::endl;
-        delete publication;
     }
 
     return 0;
