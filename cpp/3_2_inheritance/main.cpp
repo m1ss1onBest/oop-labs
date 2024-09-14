4e94cd627829786d2fb59d2006d1febf74e2d381
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-09-14 18:47:30 +0300

    add/book-class

--- cpp/3_2_inheritance/main.cpp
+++ cpp/3_2_inheritance/main.cpp
@@ -1,6 +1,24 @@
+#include <chrono>
 #include <iostream>
+#include <string>
+
+class PrintPublication {
+protected:
+    std::pmr::string title;
+    int year;
+
+public:
+    PrintPublication(const std::string& title, int year)
+        : title(title), year(year) {
+
+    }
+
+    virtual ~PrintPublication() = default;
+    virtual void printDetails() const {
+        std::cout << "Title: " << title << ", Year: " << year << std::endl;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
