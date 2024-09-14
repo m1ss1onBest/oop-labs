019dbda15d2673efe6a9de0f277efde30598c66e
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-09-14 19:00:03 +0300

    add/all-required-classes

--- cpp/3_2_inheritance/main.cpp
+++ cpp/3_2_inheritance/main.cpp
@@ -8,6 +8,7 @@ protected:
     int year;
 
 public:
+    PrintPublication() : title("Title"), year(0) {}
     PrintPublication(const std::string& title, int year):
     title(title),
     year(year) {
@@ -24,7 +25,6 @@ public:
 class Book : public PrintPublication {
     std::string author;
 public:
-
     Book(const std::string& title, int year, const std::string& author) :
     PrintPublication(title, year),
     author(author) {
@@ -41,7 +41,7 @@ class Textbook : public Book {
     std::string subject;
 
 public:
-    Textbook(const std::string& title, int year, std::string& author, const std::string& subject) :
+    Textbook(const std::string& title, int year, const std::string& author, const std::string& subject):
     Book(title, year, author),
     subject(subject) {
 
@@ -53,5 +53,34 @@ public:
     }
 };
 
+class Magazine : public PrintPublication {
+    int issueNumber;
+public:
+    Magazine(const std::string& title, int year, int issueNumber):
+    PrintPublication(title, year),
+    issueNumber(issueNumber) {
+
+    }
+
+    void printDetails() const override {
+        PrintPublication::printDetails();
+        std::cout << "Issua Number: " << issueNumber << std::endl;
+    }
+};
+
 int main() {
+    PrintPublication* publications[4] {
+        new Book("Harry Harrington", 1991, "John Wednesday"),
+        new Textbook("Introduction to Algorithms", 2009, "Thomas H Cormen", "Algorithms"),
+        new Magazine("National Geographic", 2024, 9),
+        new PrintPublication("Generic Publication", 2024)
+    };
+
+    for (auto & publication : publications) {
+        publication->printDetails();
+        std::cout << std::endl;
+        delete publication;
+    }
+
+    return 0;
 }
