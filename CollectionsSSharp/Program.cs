2a7efbb89793ff2e5bf692ccf2ebdf42c3f3034c
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-10-30 10:00:22 +0200

    slkjsldfjj

---
+++ CollectionsSSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CollectionsSSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        void print(SortedSet<int> collection)
+        {
+            foreach (var i in collection)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+        
+        
+        SortedSet<int> numbers = new SortedSet<int>();
+
+        numbers.Add(5);
+        numbers.Add(10);
+        numbers.Add(20);
+        numbers.Remove(5);
+        
+        Console.WriteLine(numbers.Contains(20));
+        Console.WriteLine("Hello, World!");
+    }
+}
